54b26eb5ad5da0a13f76cb2cfd4dcba3359d785b
Davit Mamrikishvili <davit.mamrikishvili1@gmail.com>
2025-04-26 23:50:29 +0400

    Fix Scraper

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from typing import Generator
 from dotenv import load_dotenv
 from utils.utils import get_walmart_product_id
 
@@ -14,7 +15,7 @@ class Scraper:
         self._base_url = "https://ecommerce.api.zenrows.com/v1/targets/walmart/reviews/"
         self._session = requests.Session()
     
-    def extract_reviews(self, url: str, count: int = 100, sort: str = "relevancy"):
+    def extract_reviews(self, url: str, count: int = 100, sort: str = "relevancy") -> Generator:
         params = {
             "apikey": self._api_key,
             "sort": sort,
@@ -27,15 +28,15 @@ class Scraper:
             yield review["review_content"]
             review_count -= 1
             if review_count == 0:
-                break
-        
+                return
+            
         next_page = response["pagination"].get("next_page")
         while next_page and review_count > 0:
-            params["path"] = next_page
-            response = self._session.get(zenrows_url, params=params).json()
+            params["url"] = next_page
+            response = self._session.get(self._base_url, params=params).json()
             for review in response["product_reviews_list"]:
                 yield review["review_content"]
                 review_count -= 1
                 if review_count == 0:
-                    break
+                    return
             next_page = response["pagination"].get("next_page")
