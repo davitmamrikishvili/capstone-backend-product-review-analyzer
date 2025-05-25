1e7e3a9b415391038248e1fca7ee4f9f4d8682d2
Davit Mamrikishvili <davit.mamrikishvili1@gmail.com>
2025-05-25 04:06:52 +0400

    Fix CORS issues

--- api/api.py
+++ api/api.py
@@ -11,12 +11,21 @@ from service.service import (
     reviews_to_csv,
     summarize_reviews,
 )
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(
     title="Review Analysis API",
     description="API for scraping and analyzing product reviews",
     version="1.0.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
